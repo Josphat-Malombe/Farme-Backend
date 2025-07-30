4e8f2a91932b92c416adf446e1fea062bc2d2bdb
Josphat-Malombe <musyokajosphat10@gmail.com>
2025-07-30 16:32:29 +0300

    Update settings.py
    just changed the allowed hosts

--- djbackend/settings.py
+++ djbackend/settings.py
@@ -38,10 +38,10 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
-#ALLOWED_HOSTS = ['farme-backend.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['farme-backend.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
